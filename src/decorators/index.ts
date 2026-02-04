e4e8ae61a9b7a6083e5be252b01cc98213b21838
Asif Anjum <asif.anjum.rabi@gmail.com>
2026-02-04 21:54:11 +0600

    class decorators

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -46,3 +46,59 @@ person.greet("Hello");
 const greet = person.greet;
 
 greet("Hola "); // 'this' is lost here
+
+type Post = {
+  title: string;
+  content: string;
+};
+
+console.log(`Author Class`);
+
+function addDefaultPost<This, Value extends Post[]>(
+  target: undefined,
+  context: ClassFieldDecoratorContext<This, Value>,
+) {
+  return function (initialValue: Value) {
+    initialValue.push({
+      title: "Default Post",
+      content: "This is a default post",
+    });
+
+    return initialValue;
+  };
+}
+
+function addGreetMethod<T extends new (...args: any[]) => GreetAble>(
+  target: T,
+  context: ClassDecoratorContext<T>,
+) {
+  return class extends target {
+    constructor(...args: any[]) {
+      super(...args);
+      this.greet = (greetings: string) => {
+        console.log(`${greetings}, ${this.name}! Have a great day.`);
+      };
+    }
+  };
+}
+
+class GreetAble {
+  constructor(public name: string) {}
+
+  greet: (greetings: string) => void = () => {};
+}
+
+@addGreetMethod
+class Author extends GreetAble {
+  @addDefaultPost
+  public posts: Post[] = [];
+  constructor(public name: string) {
+    super(name);
+  }
+}
+
+const author = new Author("Asif");
+
+console.log(author.posts);
+
+author.greet("Hola");
