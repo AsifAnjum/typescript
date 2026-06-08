1cac597737301833a8266b431e66c0f2958e3dea
Asif Anjum <asif.anjum.rabi@gmail.com>
2026-06-08 08:52:06 +0600

    class decorator

---
+++ src/recap/decorator_v2.ts
@@ -0,0 +1,99 @@
+import "reflect-metadata";
+
+// class decorator
+const METADATA_KEY = {
+  TABLE: Symbol("table"),
+  INJECTABLE: Symbol("injectable"),
+  SINGLETON: Symbol("singleton"),
+  
+}
+
+function Entity(tableName: string){
+  return function<T extends {new (...args:any[]): {}}>(constructor: T){
+    Reflect.defineMetadata(METADATA_KEY.TABLE, tableName, constructor);
+
+    return class extends constructor {
+      readonly _tableName = tableName;
+      readonly _createdAt = new Date().toISOString();
+    }
+  }
+}
+
+function Singleton<T extends {new (...args:any[]): {}}>(constructor: T){
+  let instance: InstanceType<T> | null = null
+  const SingletonClass =  class extends constructor {
+    constructor(...args: any[]){
+      if(instance){
+        return instance;
+      }
+      super(...args);
+      instance = this as any;
+    }
+  }
+
+  Object.defineProperty(SingletonClass, 'instance', {
+    value: constructor.name
+  })
+
+  return SingletonClass;
+}
+
+
+function Timestamped<T extends {new (...args:any[]): {}}>(Base:T){
+
+  return class extends Base {
+    createdAt = new Date()
+    private _updatedAt = new Date()
+    
+    get updatedAt() {
+      return this._updatedAt
+    }
+
+    touch() {
+      this._updatedAt = new Date();
+      return this;
+    }
+  }
+}
+
+@Entity('employees')
+@Timestamped
+class Employee {
+  constructor(
+    public id: string,
+    public name: string,
+    public role: "ADMIN" | "USER"
+  ) {}
+}
+
+@Singleton
+class DataBaseConnection {
+  private static count = 0;
+  readonly connectionId: number;
+
+  constructor(public url: string){
+    DataBaseConnection.count++;
+    this.connectionId = DataBaseConnection.count;
+    console.log(`Database connected to ${url} with ID ${this.connectionId}`);
+  }
+}
+
+
+const emp = new Employee("e-001", "Asif", "ADMIN");
+console.log((emp as any)._tableName);
+console.log((emp as any)._createdAt);
+(emp as any).touch();
+console.log((emp as any).updatedAt);
+
+const db1 = new DataBaseConnection("localhost:5432");
+const db2 = new DataBaseConnection("localhost:5432");
+console.log(db1 === db2);
+console.log(db1.connectionId)
+console.log(db2.connectionId)
+
+
+//read meta data
+const table = Reflect.getMetadata(METADATA_KEY.TABLE, Employee);
+console.log(table);
+
+
