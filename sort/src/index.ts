a4e989d49267c80a43317e3f3f71005a0a91dc70
Asif Anjum <asif.anjum.rabi@gmail.com>
2026-06-14 08:38:09 +0600

    feat: initialize TypeScript project with configuration, source directory, and package structure

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello World");
+class Sorter {
+    constructor(public collection: number[]| string){}
+
+    public bubble_sort(): void {
+        const {length} = this.collection;
+
+        for(let i = 0; i < length; i++){
+            for(let j = 0; j < length - i -1; j++){
+                if(this.collection instanceof Array && this.collection[j] > this.collection[j+1]){
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j+1];
+                    this.collection[j+1] = leftHand;   
+                }
+
+                if(typeof this.collection === 'string'){
+                    const leftHandCharCode = this.collection.charCodeAt(j);
+                    const rightHandCharCode = this.collection.charCodeAt(j+1);
+
+                    
+                }
+
+            }
+        }
+    }
+
+}
+
+const sorter = new Sorter([10,3,-5,0]);
+sorter.bubble_sort();
+console.log(sorter.collection);
+
+const sorter2 = new Sorter('Xayb');
+sorter2.bubble_sort();
+console.log(sorter2.collection);
