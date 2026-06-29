bf631f2aff04130d3a550b71713a694e94620934
AsifAnjum <asif.anjum.rabi@gmail.com>
2026-06-29 17:14:44 +0600

    feat: implement User model with event handling system

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -2,7 +2,26 @@ import { User } from "./models/User";
 
 const user = new User({ name: "John", age: 30 });
 
-user.set({ name: "Jane" });
+// user.set({ name: "Jane" });
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+// console.log(user.get("name"));
+// console.log(user.get("age"));
+
+user.on('change', () => {
+    console.log('change #1')
+})
+
+user.on('change', () => {
+    console.log('change #2')
+})
+
+user.on('save', () => {
+
+    console.log('save #1')
+})
+
+
+user.trigger('change')
+user.trigger('save')
+
+console.log(user)
--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -3,7 +3,11 @@ interface UserProps {
     age?: number;
 }
 
+type Callback = () => void
+
 export class User {
+    public events: { [key: string]: Callback[] } = {};
+
     constructor(private data: UserProps) { }
 
     get(propertyName: keyof UserProps): UserProps[keyof UserProps] {
@@ -12,6 +16,24 @@ export class User {
 
     set(updateProperties: UserProps): void {
         Object.assign(this.data, updateProperties);
+    }
+
+    public on(eventName: string, callback: Callback): void {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
+
+    public trigger(eventName: string): void {
+        const handlers = this.events[eventName];
+
+        if (!handlers || handlers.length === 0) {
+            return;
+        }
+
+        handlers.forEach(callback => {
+            callback();
+        })
     }
 
 }
